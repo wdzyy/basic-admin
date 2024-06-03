f52ab78c90858678c19862391b50c23f4cf7870c
QJia <532612154@qq.com>
2024-06-03 09:15:30 +0800

    chore(components.ts): 由于component :is无法动态导入组件，所以全局导入element-plus，因此取消动态导入ui组件

--- vite/plugins/components.ts
+++ vite/plugins/components.ts
@@ -1,5 +1,6 @@
 import components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// 全局引入了element-plus，所以不需要再引入
+// import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default function createComponents() {
   return components({
@@ -9,10 +10,10 @@ export default function createComponents() {
     ],
     include: [/\.vue$/, /\.vue\?vue/, /\.tsx$/],
     dts: './src/types/components.d.ts',
-    resolvers: [
-      ElementPlusResolver({
-        importStyle: 'sass',
-      }),
-    ],
+    // resolvers: [
+    //   ElementPlusResolver({
+    //     importStyle: 'sass',
+    //   }),
+    // ],
   })
 }
